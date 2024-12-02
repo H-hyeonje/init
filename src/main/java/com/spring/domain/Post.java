1ee3ba40db67d7efe6b0a36f408b9639734bd936
H-hyeonje <guswpwkf@naver.com>
2024-12-02 11:37:25 +0900

    []

--- src/main/java/com/spring/domain/Post.java
+++ src/main/java/com/spring/domain/Post.java
@@ -19,4 +19,97 @@ public class Post
 	private Dining dining;
 	private Landmark landmark;
 	private Festival festival;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Date getPublishDate() {
+		return publishDate;
+	}
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
+	public int getView() {
+		return view;
+	}
+	public void setView(int view) {
+		this.view = view;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public boolean isPrivate() {
+		return isPrivate;
+	}
+	public void setPrivate(boolean isPrivate) {
+		this.isPrivate = isPrivate;
+	}
+	public int getUnique() {
+		return unique;
+	}
+	public void setUnique(int unique) {
+		this.unique = unique;
+	}
+	public Date getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(Date commentDate) {
+		this.commentDate = commentDate;
+	}
+	public int getCommentLikes() {
+		return commentLikes;
+	}
+	public void setCommentLikes(int commentLikes) {
+		this.commentLikes = commentLikes;
+	}
+	public int getSatisfaction() {
+		return satisfaction;
+	}
+	public void setSatisfaction(int satisfaction) {
+		this.satisfaction = satisfaction;
+	}
+	public Dining getDining() {
+		return dining;
+	}
+	public void setDining(Dining dining) {
+		this.dining = dining;
+	}
+	public Landmark getLandmark() {
+		return landmark;
+	}
+	public void setLandmark(Landmark landmark) {
+		this.landmark = landmark;
+	}
+	public Festival getFestival() {
+		return festival;
+	}
+	public void setFestival(Festival festival) {
+		this.festival = festival;
+	}
+	
+	
+	
 }
